261f728d5053577323c0abd4877b64cc4ffe471f
Dino <dattimbol@mymail.mapua.edu.ph>
2025-02-27 22:34:11 +0800

    Made Login and Signup pages (Responsive)

---
+++ signup.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html" charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="Content" content="Project 2 : Web Design and Development">
+    <meta name="Page" content="Sign Up Page">
+
+    <!--  Bootstrap stylesheet link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+    <!-- local CSS stylesheet link -->
+    <link rel="stylesheet" href="./css/signup.css">
+
+    <link rel="icon" href="./images/logo.png">
+
+    <link href='https://fonts.googleapis.com/css?family=Open Sans' rel='stylesheet'>
+
+    <title>Sign Up</title>
+
+</head>
+
+<body>
+
+    <section class="container-fluid p-3 d-flex align-items-center">
+        <div class="logo me-3">
+            <img src="./images/logo.png" alt="San Antonio Logo" width="75px" height="75px">
+        </div>
+
+        <div class="headTitle">
+            <h2 class="mb-0" style="color: black"><b>BARANGAY SAN ANTONIO | MAKATI CITY</b></h2>
+            <h3 class="mb-0" style="color: white">EMPLOYEE MANAGEMENT SYSTEM</h3>
+        </div>
+    </section>
+
+    <section class="signup-center">
+        <div class="signup-container text-center">
+            <h2 class="mb-4"><b>Sign Up</b></h2>
+            <form>
+                <div class="mb-3">
+                    <input type="text" class="form-control" style="background-color: lightblue;" placeholder="Email"
+                        required>
+                </div>
+                <div class="mb-3">
+                    <input type="text" class="form-control" style="background-color: lightblue;"
+                        placeholder="Employee ID" required>
+                </div>
+                <div class="mb-3 password-container">
+                    <input type="password" id="password" class="form-control" style="background-color: lightblue;"
+                        placeholder="Password" required>
+                    <span class="toggle-password" onclick="togglePassword()">👁️</span>
+                </div>
+                <div class="mb-3 password-container">
+                    <input type="password" id="password" class="form-control" style="background-color: lightblue;"
+                        placeholder="Confirm Password" required>
+                </div>
+                <button type="submit" class="btn btn-primary btn-signup">Sign Up</button>
+            </form>
+        </div>
+    </section>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+
+    <script src="./js/signup.js"></script>
+
+</body>
+
+</html>
